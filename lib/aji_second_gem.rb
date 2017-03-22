fe12febc61982e8da55132a57e99a7abeea4df77
ajith <ajith@codingmart.com>
2017-03-22 17:29:13 +0530

    all

---
+++ lib/aji_second_gem.rb
@@ -0,0 +1,12 @@
+require "aji_second_gem/version"
+
+module AjiSecondGem
+  class gem
+  	def hi
+  		puts "hiiiii..."
+  	end
+  	def hi(name)
+  		puts "hi #{name} welcome to coding world"
+  	end
+  end
+end
---
+++ lib/aji_second_gem/version.rb
@@ -0,0 +1,3 @@
+module AjiSecondGem
+  VERSION = "0.1.0"
+end
